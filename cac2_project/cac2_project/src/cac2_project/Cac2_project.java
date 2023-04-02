96660a8235fa8f2af9b0241d7de32439937c7fd6
jagritirai223 <117372332+jagritirai223@users.noreply.github.com>
2023-04-02 22:06:38 +0530

    Add files via upload

---
+++ cac2_project/cac2_project/src/cac2_project/Cac2_project.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cac2_project;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Cac2_project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        new login().setVisible(true);
+    }
+    
+}
